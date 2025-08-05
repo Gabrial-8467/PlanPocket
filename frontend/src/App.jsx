9b78319c6c94eedcc2e8a9a87778399a672a7280
gabrialdeora <gabrialdeora004@gmail.com>
2025-08-05 14:47:55 +0530

    changes committed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container w-100 h-100 bg-red-300">
+      <div className="text-3xl text-center underline font-bold border-2 border-black flex justify-center items-center">Hello World!!!</div>
+    </div>
+  );
+}
+
+export default App;
